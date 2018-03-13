eed3098b15f73553312f1a577399cfe22ed80b9f
davidhigginbotham <dhigginbotham@clearent.com>
2018-03-13 10:30:39 -0500

    add url and public key to protocol. implement asynchronous call correctly

---
+++ ClearentPayments/ClearentConfiguration.h
@@ -0,0 +1,13 @@
+//
+//  ClearentConfiguration.h
+//  ClearentPayments
+//
+//  Created by David Higginbotham on 3/13/18.
+//  Copyright © 2018 Clearent, L.L.C. All rights reserved.
+//
+
+#ifndef ClearentConfiguration_h
+#define ClearentConfiguration_h
+
+
+#endif /* ClearentConfiguration_h */
---
+++ ClearentPayments/ClearentConfiguration.m
@@ -0,0 +1,9 @@
+//
+//  ClearentConfiguration.m
+//  ClearentPayments
+//
+//  Created by David Higginbotham on 3/13/18.
+//  Copyright © 2018 Clearent, L.L.C. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
