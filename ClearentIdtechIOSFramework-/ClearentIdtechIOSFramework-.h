2e331aa87b00f7fb0dea869aa84ad1d83671fb77
davidhigginbotham <dhigginbotham@clearent.com>
2018-05-03 14:19:15 -0500

    still trying

---
+++ ClearentIdtechIOSFramework-/ClearentIdtechIOSFramework-.h
@@ -0,0 +1,9 @@
+//
+//  ClearentIdtechIOSFramework-.h
+//  ClearentIdtechIOSFramework-
+//
+//  Created by David Higginbotham on 5/3/18.
+//  Copyright © 2018 Clearent, L.L.C. All rights reserved.
+//
+
+#import <ClearentIdtechIOSFramework-/Clearent_UniPayIII.h>
--- ClearentIdtechIOSFramework-/ClearentPayments.h
+++
@@ -1,1 +0,0 @@
-#import <ClearentIdtechIOSFramework-/Clearent_UniPayIII.h>
