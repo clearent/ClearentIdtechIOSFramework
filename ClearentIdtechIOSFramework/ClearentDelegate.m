50f7660451b6095bb2b63e122947f5c9898ed856
davidhigginbotham <dhigginbotham@clearent.com>
2018-06-04 11:20:44 -0500

    dont send theser tags

--- ClearentIdtechIOSFramework/ClearentDelegate.m
+++ ClearentIdtechIOSFramework/ClearentDelegate.m
@@ -205,8 +205,8 @@ BOOL isSupportedEmvEntryMode (int entryMode) {
     //Get tags based on TSYS impl guide. TODO Rely on on what is returned from emv_retrieveTransactionResult
     //TODO CONTACTLESS 9F6D 9F66
     
-    //leave out 9F10, Danny says leave these too since they are not a part of AUTH (capture only tags) 9F0D, 9F0E, 9F0F
-    NSData *tsysTags = [IDTUtility hexToData:@"82959A9B9C5F2A9F029F039F1A9F219F269F279F339F349F359F369F379F394F845F2D5F349F069F09DF78DF799F155F369F1B9F1E9F1C9F4E5A9F6E9F5357"];
+    //leave out 9F10, Danny says leave these too since they are not a part of AUTH (capture only tags) 9F0D, 9F0E, 9F0F. DF26, 9F53, and 9F4E
+    NSData *tsysTags = [IDTUtility hexToData:@"82959A9B9C5F2A9F029F039F1A9F219F269F279F339F349F359F369F379F394F845F2D5F349F069F09DF78DF799F155F369F1B9F1E9F1C5A9F6E57"];
     
     //good ones confirmed!
     //57 5a 9f1a 9c 95 9f03 9f15 9f27 9f39 df79 9f0d 9f35 9f1b 5f34 9f0e 9f36 9f1c 9f40 9f09 9f4e 5f2d 9f0f 9f21 9f33 82 4F 5f36 9f06 5f2a 9f02 9f26 84 9B 9F1E 9F34 DF78
@@ -232,7 +232,7 @@ BOOL isSupportedEmvEntryMode (int entryMode) {
         //majors
         [retrievedResultTags setObject:@"6028C8" forKey:@"9F33"];
         [retrievedResultTags setObject:@"F000F0A001" forKey:@"9F40"];
-        [retrievedResultTags setObject:@"01" forKey:@"DF26"];
+       // [retrievedResultTags setObject:@"01" forKey:@"DF26"];
         
         //Set Minor Tags
         [retrievedResultTags setObject:@"02" forKey:@"5F36"];
@@ -240,7 +240,7 @@ BOOL isSupportedEmvEntryMode (int entryMode) {
         [retrievedResultTags setObject:@"5465726D696E616C" forKey:@"9F1E"];
         [retrievedResultTags setObject:@"5999" forKey:@"9F15"];
         [retrievedResultTags setObject:@"888000001516" forKey:@"9F16"];
-        [retrievedResultTags setObject:@"54657374204d65726368616e74" forKey:@"9F4E"];
+        //[retrievedResultTags setObject:@"54657374204d65726368616e74" forKey:@"9F4E"];
         
         //Terminal Version Number
         [retrievedResultTags setObject:@"5999" forKey:@"9F15"];
