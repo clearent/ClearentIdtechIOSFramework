dcc962eaf0e12c9695869c6b264999381d1065ed
Ovidiu Pop <ovidiu.pop@endava.com>
2022-03-28 17:19:32 +0300

    Removed some unnecessary classes.

--- ClearentIdtechIOSFramework/ClearentIdtechIOSFramework.h
+++ ClearentIdtechIOSFramework/ClearentIdtechIOSFramework.h
@@ -22,8 +22,6 @@
 #import "ClearentFeedback.h"
 #import "ClearentBluetoothDevice.h"
 #import "ClearentDeviceConnector.h"
-#import "ClearentUIManager.h"
-#import <ClearentIdtechIOSFramework/ClearentIdtechIOSFramework-Swift.h>
 
 #import "IDT_VP3300.h"
 #import "IDTCommon.h"
