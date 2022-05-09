7fcfdb3a043795a2b39ef480331ee22f8f7f94f5
Ovidiu Rotaru <ovidiu.rotaru@endava.com>
2022-05-09 14:34:56 +0300

    Changed order of items for the new case.

--- ClearentIdtechIOSFramework/ClearentUI/PaymentFlowDataSource/FlowDataProvider.swift
+++ ClearentIdtechIOSFramework/ClearentUI/PaymentFlowDataSource/FlowDataProvider.swift
@@ -148,10 +148,10 @@ extension FlowDataProvider : ClearentWrapperProtocol {
                      FlowDataItem(type: .graphicType, object: FlowGraphicType.insert_card)]
         case .tapFailed:
             type = .warning
-            items = [FlowDataItem(type: .title, object: "xsdk_read_error_title".localized),
+            items = [FlowDataItem(type: .graphicType, object: FlowGraphicType.insert_card),
+                     FlowDataItem(type: .title, object: "xsdk_read_error_title".localized),
                      FlowDataItem(type: .description, object: "xsdk_tap_failed_insert_swipe_description".localized),
-                     FlowDataItem(type: .userAction, object: FlowButtonType.cancel),
-                     FlowDataItem(type: .graphicType, object: FlowGraphicType.insert_card)]
+                     FlowDataItem(type: .userAction, object: FlowButtonType.cancel)]
         case .removeCard, .cardSecured:
             print("nothing to do here")
         case .transactionStarted, .goingOnline:
