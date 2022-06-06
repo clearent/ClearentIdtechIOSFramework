f0bf97773fa8710a88a536caea27687059618793
carmenjurcovan <carmen_jurco@yahoo.com>
2022-06-06 12:19:30 +0300

    swiftformat

--- ClearentIdtechIOSFramework/ClearentUI/Common/UIComponents/ClearentHintView.swift
+++ ClearentIdtechIOSFramework/ClearentUI/Common/UIComponents/ClearentHintView.swift
@@ -9,15 +9,13 @@
 import UIKit
 
 public class ClearentHintView: ClearentMarginableView {
-    
-    // MARK: - Outlets
-    
-    @IBOutlet weak var label: PaddingLabel!
-    @IBOutlet weak var bubbleTail: UIImageView!
-    @IBOutlet weak var stackView: UIStackView!
 
     // MARK: - Properties
-    
+
+    @IBOutlet var label: PaddingLabel!
+    @IBOutlet var bubbleTail: UIImageView!
+    @IBOutlet var stackView: UIStackView!
+
     override public var margins: [BottomMargin] {
         [
             RelativeBottomMargin(constant: 40, relatedViewType: ClearentIcon.self),
@@ -26,13 +24,13 @@ public class ClearentHintView: ClearentMarginableView {
             RelativeBottomMargin(constant: 45, relatedViewType: ClearentPairingReadersList.self)
         ]
     }
-    
+
     public var title: String? {
         didSet {
             label.text = title
         }
     }
-    
+
     public var highlightedTextColor = ClearentConstants.Color.backgroundSecondary01 {
         didSet {
             label.textColor = highlightedTextColor
@@ -44,25 +42,25 @@ public class ClearentHintView: ClearentMarginableView {
             label.textColor = defaultTextColor
         }
     }
-    
+
     public var highlightedBackgroundColor = ClearentConstants.Color.accent03 {
         didSet {
             updateAppearance()
         }
     }
-    
+
     public var textFont: UIFont? {
         didSet {
             label.font = textFont
         }
     }
-    
+
     public var isHighlighted: Bool = false {
         didSet {
             updateAppearance()
         }
     }
-    
+
     public var bubbleTailIsOnTop: Bool = false {
         didSet {
             bubbleTail.transform = bubbleTail.transform.rotated(by: .pi)
@@ -70,7 +68,7 @@ public class ClearentHintView: ClearentMarginableView {
             stackView.insertArrangedSubview(bubbleTail, at: 0)
         }
     }
-    
+
     convenience init(text: String?) {
         self.init()
         label.text = text
@@ -83,26 +81,26 @@ public class ClearentHintView: ClearentMarginableView {
         setupBubbleTail()
         setupLabel()
     }
-    
+
     // MARK: - Private
-    
+
     private func setupLabel() {
         textFont = ClearentConstants.Font.proTextNormal
         label.layer.cornerRadius = 10
         label.layer.masksToBounds = true
         label.clipsToBounds = true
     }
-    
+
     private func setupBubbleTail() {
         if isHighlighted {
             bubbleTail.image = UIImage(named: ClearentConstants.IconName.bubbleTail, in: ClearentConstants.bundle, with: nil)
         }
     }
-    
+
     private func updateAppearance() {
         label.backgroundColor = isHighlighted ? highlightedBackgroundColor : .clear
         label.textColor = isHighlighted ? highlightedTextColor : defaultTextColor
-        label.textEdgeInsets = isHighlighted ? UIEdgeInsets.init(top: 8, left: 12, bottom: 8, right: 12) : .zero
+        label.textEdgeInsets = isHighlighted ? UIEdgeInsets(top: 8, left: 12, bottom: 8, right: 12) : .zero
         bubbleTail.tintColor = highlightedBackgroundColor
         bubbleTail.isHidden = !isHighlighted
     }
@@ -112,14 +110,14 @@ class PaddingLabel: UILabel {
     var textEdgeInsets: UIEdgeInsets = .zero {
         didSet { invalidateIntrinsicContentSize() }
     }
-    
-    open override func textRect(forBounds bounds: CGRect, limitedToNumberOfLines numberOfLines: Int) -> CGRect {
+
+    override open func textRect(forBounds bounds: CGRect, limitedToNumberOfLines numberOfLines: Int) -> CGRect {
         let insetRect = bounds.inset(by: textEdgeInsets)
         let textRect = super.textRect(forBounds: insetRect, limitedToNumberOfLines: numberOfLines)
         let invertedInsets = UIEdgeInsets(top: -textEdgeInsets.top, left: -textEdgeInsets.left, bottom: -textEdgeInsets.bottom, right: -textEdgeInsets.right)
         return textRect.inset(by: invertedInsets)
     }
-    
+
     override func drawText(in rect: CGRect) {
         super.drawText(in: rect.inset(by: textEdgeInsets))
     }
