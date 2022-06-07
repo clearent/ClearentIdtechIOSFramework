cd7584b98e736fbe34f70640fca776ccad1898a2
Ovidiu Rotaru <ovidiu.rotaru@endava.com>
2022-06-07 14:00:17 +0300

    This is not correct.

--- ClearentIdtechIOSFramework/Wrapper/ClearentWrapper.swift
+++ ClearentIdtechIOSFramework/Wrapper/ClearentWrapper.swift
@@ -452,10 +452,8 @@ extension ClearentWrapper : Clearent_Public_IDTech_VP3300_Delegate {
     
     public func deviceDisconnected() {
         DispatchQueue.main.async {
-            if !self.searchingRecentlyUsedReadersInProgress {
-                ClearentWrapperDefaults.pairedReaderInfo?.isConnected = false
-                self.delegate?.deviceDidDisconnect()
-            }
+            ClearentWrapperDefaults.pairedReaderInfo?.isConnected = false
+            self.delegate?.deviceDidDisconnect()
         }
     }
 }
