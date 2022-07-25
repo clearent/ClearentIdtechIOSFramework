c1593fd46307c9e6c4b263a36624b118d5204c25
Ovidiu Rotaru <ovidiu.rotaru@endava.com>
2022-07-25 14:16:16 +0300

    Added comments for Wrapper Protocol.

--- ClearentIdtechIOSFramework/Wrapper/ClearentWrapper.swift
+++ ClearentIdtechIOSFramework/Wrapper/ClearentWrapper.swift
@@ -121,21 +121,94 @@ public enum TransactionError {
     case networkError, insuficientFunds, duplicateTransaction, generalError
 }
 
+/**
+ * A protocol used to comunicate  informations and results regarding interaction with the card reader & API's
+ */
 public protocol ClearentWrapperProtocol : AnyObject {
+    /**
+     * Method called right after a pairing process is started.
+     *  We're calling a pairing process , bluetooth search start, selection of a device and connection to that device
+     */
     func didStartPairing()
+    
+    /**
+     * Method called right after a succesfull connection to a card reader was made
+     */
     func didFinishPairing()
+    
+    /**
+     * Method called as a response to 'startDeviceInfoUpdate' method and indicated that new reader information is available
+     */
     func didReceiveSignalStrength()
+    
+    /**
+     * Method called after a pairing process is started card readers were found nearby
+     * @param readers, the list of readers available for pairing
+     */
     func didFindReaders(readers:[ReaderInfo])
+    
+    /**
+     * Method called when the curently paired device disconnects
+     */
     func deviceDidDisconnect()
+    
+    /**
+     * Method called when a pairing process was started but there are no available readers to pair with nearby
+     */
     func didNotFindReaders()
+    
+    /**
+     * Method called after the 'connectTo' method was called by protocol implementing class, indicating that  a connection to the selected reader is beeing  established
+     */
     func startedReaderConnection(with reader:ReaderInfo)
+    
+    /**
+     * Method called  in response to method 'searchRecentlyUsedReaders' and indicated that recently readers were found
+     * @param readers. list of recently paired readers
+     */
     func didFindRecentlyUsedReaders(readers:[ReaderInfo])
+    
+    /**
+     * Method called  in response to method 'searchRecentlyUsedReaders' and indicated that no recently readers were found
+     */
     func didNotFindRecentlyUsedReaders()
+    
+    /**
+     * Method called  to indicate that continously search readers nearby process has started
+     */
     func didBeginContinuousSearching()
+    
+    /**
+     * Method called  when a general error is encountered in apayment/transaction process
+     */
     func didEncounteredGeneralError()
+    
+    /**
+     * Method called  when a transaction is finished
+     * @param response, transaction response as received from the API
+     * @param error, if not null will contain the error received from the API
+     */
     func didFinishTransaction(response: TransactionResponse, error: ResponseError?)
+    
+    /**
+     * Method called  when the process of uploadiing signature image has completed
+     * @param response, upload response as received from the API
+     * @param error, if not null will contain the error received from the API
+     */
     func didFinishedSignatureUploadWith(response: SignatureResponse, error: ResponseError?)
+    
+    /**
+     * Method called  each time the reader needs an action from the user,
+     * @UserAction, please check the enum for more cases
+     * @action, User Action needed to be performed by the user
+     */
     func userActionNeeded(action: UserAction)
+    
+    /**
+     * Method called  each time the reader wants to infor the user
+     * @UserInfo, please check the enum for more cases
+     * @info , UserInfo needed to be performed by the user
+     */
     func didReceiveInfo(info: UserInfo)
 }
 
