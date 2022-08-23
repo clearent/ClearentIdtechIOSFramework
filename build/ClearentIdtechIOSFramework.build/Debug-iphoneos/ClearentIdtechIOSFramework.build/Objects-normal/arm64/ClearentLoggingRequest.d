72432739f11655f2dbbc4b237881e995343d9a83
dhigginbotham <dhigginbotham@clearent.com>
2022-08-23 12:02:28 -0500

    Remove artifacts that can be generated

--- build/ClearentIdtechIOSFramework.build/Debug-iphoneos/ClearentIdtechIOSFramework.build/Objects-normal/arm64/ClearentLoggingRequest.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/dhigginbotham/Documents/_idtech_frameworks/ClearentIdtechIOSFramework/ClearentIdtechIOSFramework/Logging/ClearentLoggingRequest.m \
-  /Users/dhigginbotham/Documents/_idtech_frameworks/ClearentIdtechIOSFramework/build/ClearentIdtechIOSFramework.build/Debug-iphoneos/ClearentIdtechIOSFramework.build/module.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.5.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.5.sdk/usr/include/mach-o/dyld.modulemap \
-  /Users/dhigginbotham/Documents/_idtech_frameworks/ClearentIdtechIOSFramework/ClearentIdtechIOSFramework/Logging/ClearentLoggingRequest.h \
-  /Users/dhigginbotham/Documents/_idtech_frameworks/ClearentIdtechIOSFramework/ClearentIdtechIOSFramework/ClearentUtils.h
