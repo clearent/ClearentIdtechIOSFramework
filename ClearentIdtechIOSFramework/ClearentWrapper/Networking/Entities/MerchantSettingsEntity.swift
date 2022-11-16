0f1b8b5454d382d3bc56ae722d86e541a785e181
Ovidiu Rotaru <ovidiu.rotaru@endava.com>
2022-11-16 11:18:05 +0200

    Updated terminal settings params.

--- ClearentIdtechIOSFramework/ClearentWrapper/Networking/Entities/MerchantSettingsEntity.swift
+++ ClearentIdtechIOSFramework/ClearentWrapper/Networking/Entities/MerchantSettingsEntity.swift
@@ -20,8 +20,16 @@ struct PayloadSettings: Codable {
 
 struct TerminalSettings: Codable {
     let enableTip: Bool
-
+    let serviceFeeState : String?
+    let serviceFee : String?
+    let serviceFeeType : String?
+    let serviceFeeProgram : String?
+    
     enum CodingKeys: String, CodingKey {
         case enableTip = "enable-tip"
+        case serviceFeeState = "service-fee-state"
+        case serviceFee = "service-fee"
+        case serviceFeeType = "service-fee-type"
+        case serviceFeeProgram = "service-fee-program-type"
     }
 }
