211e8936d47dba4614dfd0f626b139f8a57adec4
Carmen Jurcovan <carmenjurcovan@EN1411418.local>
2022-12-14 14:56:21 +0200

    Add support for passing multiple values to the SDK

---
+++ ClearentIdtechIOSFramework/ClearentUI/PaymentInfo.swift
@@ -0,0 +1,25 @@
+//
+//  PaymentInfo.swift
+//  ClearentIdtechIOSFramework
+//
+//  Created by Carmen Jurcovan on 14.12.2022.
+//  Copyright © 2022 Clearent, L.L.C. All rights reserved.
+//
+
+@objc public class PaymentInfo: NSObject {
+    public var amount: Double
+    public var customerID: String?
+    public var invoice: String?
+    public var orderID: String?
+    public var billing: ClientInformation?
+    public var shipping: ClientInformation?
+    
+    public init(amount: Double, customerID: String? = nil, invoice: String? = nil, orderID: String? = nil, billing: ClientInformation? = nil, shipping: ClientInformation? = nil) {
+        self.amount = amount
+        self.customerID = customerID
+        self.invoice = invoice
+        self.orderID = orderID
+        self.billing = billing
+        self.shipping = shipping
+    }
+}
