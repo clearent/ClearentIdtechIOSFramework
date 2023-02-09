c319423e9dd7d31de5593bb29f3390aee8a334c0
Carmen Jurcovan <carmenjurcovan@EN1411418.local>
2023-02-09 18:27:06 +0200

    Code cleanup

--- ClearentIdtechIOSFramework-Tests/ClearentUI/Helper/ClearentMarginableExtension.swift
+++ ClearentIdtechIOSFramework-Tests/ClearentUI/Helper/ClearentMarginableExtension.swift
@@ -20,24 +20,3 @@ extension ClearentMarginable {
         XCTAssertEqual(bottomMargin.constant, margin)
     }
 }
-
-
-extension OfflineModeManager {
-    func generateOfflineTransactions(count: Int, cardReaderTransactions: Bool) {
-        for index in 1...count {
-            let amount = "\(index).00"
-            
-            // manual entry transactions
-            var cardSaleEntity = SaleEntity(amount: amount, card: "4111111111111111", csc: "999", expirationDateMMYY: "1132")
-            var paymentData = PaymentData(saleEntity: cardSaleEntity)
-            
-            if cardReaderTransactions {
-                // card reader transactions
-                cardSaleEntity = SaleEntity(amount: amount)
-                paymentData = PaymentData(saleEntity: cardSaleEntity, cardToken: "some_secret_token_here".data(using: .utf8))
-            }
-            let oftr = OfflineTransaction(paymentData: paymentData)
-            _ = saveOfflineTransaction(transaction: oftr)
-        }
-    }
-}
---
+++ ClearentOfflineManagerExtension.swift
@@ -0,0 +1,27 @@
+//
+//  ClearentOfflineManagerExtension.swift
+//  ClearentIdtechIOSFramework
+//
+//  Created by Carmen Jurcovan on 09.02.2023.
+//  Copyright © 2023 Clearent, L.L.C. All rights reserved.
+//
+
+extension OfflineModeManager {
+    func generateOfflineTransactions(count: Int, cardReaderTransactions: Bool) {
+        for index in 1...count {
+            let amount = "\(index).00"
+            
+            // manual entry transactions
+            var cardSaleEntity = SaleEntity(amount: amount, card: "4111111111111111", csc: "999", expirationDateMMYY: "1132")
+            var paymentData = PaymentData(saleEntity: cardSaleEntity)
+            
+            if cardReaderTransactions {
+                // card reader transactions
+                cardSaleEntity = SaleEntity(amount: amount)
+                paymentData = PaymentData(saleEntity: cardSaleEntity, cardToken: "some_secret_token_here".data(using: .utf8))
+            }
+            let oftr = OfflineTransaction(paymentData: paymentData)
+            _ = saveOfflineTransaction(transaction: oftr)
+        }
+    }
+}
