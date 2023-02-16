8faa32adb3c45ffd3da3fcb3d00e6ae619dd096b
Carmen Jurcovan <carmenjurcovan@EN1411418.local>
2023-02-16 13:26:08 +0200

    Removed debug code

--- ClearentIdtechIOSFramework/ClearentWrapper/Wrapper/ClearentWrapper.swift
+++ ClearentIdtechIOSFramework/ClearentWrapper/Wrapper/ClearentWrapper.swift
@@ -552,7 +552,7 @@ extension ClearentWrapper: Clearent_Public_IDTech_VP3300_Delegate {
     
     public func successOfflineTransactionToken(_ clearentTransactionTokenRequestData: Data?, isTransactionEncrypted isEncrypted: Bool) {
         guard let cardToken = clearentTransactionTokenRequestData else { return }
-        let isEncrypted = true
+
         ClearentWrapperDefaults.pairedReaderInfo?.encrypted = isEncrypted
         if (!isEncrypted) {
             self.delegate?.showEncryptionWarning()
